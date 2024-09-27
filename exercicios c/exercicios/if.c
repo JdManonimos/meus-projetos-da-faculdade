6a667db19daac036eb380190318015dee11da2d3
Mateus Felipe <108820978+JdManonimos@users.noreply.github.com>
2024-09-26 23:33:39 -0300

    Add files via upload

---
+++ exercicios c/exercicios/if.c
@@ -0,0 +1,10 @@
+int main() {
+    int x = 10;
+    if(x>5){
+        printf("x e maior que 5\n");
+    }
+        else {
+            printf("x e menor ou igual a 5\n");
+        }
+        return 0;
+    }
